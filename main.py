196c56a29af6d0e8565c00b461ce8df3e8bb2464
Abhishek.kothari <abko27700@gmail.com>
2023-02-22 07:52:06 +0530

    Basic understanding of routing

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # Everyy time there is a change in the code, the file will be rerun automatically.
 
