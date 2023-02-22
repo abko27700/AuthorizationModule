2a06ce027659c81eb86a0b88a6b333c19ee6411b
Abhishek.kothari <abko27700@gmail.com>
2023-02-22 08:27:56 +0530

    Ui forsignup, login

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", text="Abhish testing", user="")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/sign-up')
 def signup():
-    return "<p>Signup</p>"
+    return render_template("sign_up.html")
 
 
